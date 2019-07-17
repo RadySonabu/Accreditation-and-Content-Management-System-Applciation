efbcd67974893431d1b441fcf665d8a90ae21e5f
RadySonabu <ardynubanos@yahoo.com>
2019-07-17 22:05:24 +0800

    Hierarchical

--- geo/dashboard/views.py
+++ geo/dashboard/views.py
@@ -5,9 +5,19 @@ from members.models import MyUser
 
 
 def home(request):
+    user = request.user
+    if user.course == 'VPAA':
+        context = {
+            'members': MyUser.objects.filter(course='Dean')
+        }
+    elif user.course == 'Dean':
+        context = {
+            'members': MyUser.objects.filter(course='DC')
+        }
+    elif user.course == 'DC':
+        context = {
 
-    context = {
-        'members': MyUser.objects.filter(course='Dean')
-    }
-
+        }
+    else:
+        f'Non admin'
     return render(request, 'dashboard/home.html', context)
