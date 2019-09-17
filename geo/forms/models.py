2281beda0abeba760adbcecb5caf9546c71f9f87
heisiapo <heisiapco@gmail.com>
2019-09-17 14:18:18 +0800

    ardy

--- geo/forms/models.py
+++ geo/forms/models.py
@@ -21,7 +21,7 @@ class AccreditationType(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 200 or img.width > 200:
+        if img.height != 200 or img.width != 200:
             output_size = (200, 200)
             img.thumbnail(output_size)
             img.save(self.image.path)
